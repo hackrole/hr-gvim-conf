e0d3785ce25416ba49ec93791378f40741fcccaa
hackrole <daipeng123456@gmail.com>
2016-08-24 16:47:37 +0800

    fix it

--- tasks.py
+++ tasks.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from invoke import task, run
 
 
+DEBUG = os.environ.get('HR_DEBUG', False)
 # project base dir
 BASE_DIR = path.dirname(path.realpath(__file__))
 # logging config
@@ -20,52 +21,63 @@ def now_str():
     return datetime.strftime(datetime.now(), fmt)
 
 
+def path_exists(path_str):
+    return path.exists(path.expanduser(path_str))
+
+
+def run_cmd(cmd):
+    if DEBUG:
+        logging.info(cmd)
+    else:
+        run_cmd(cmd)
+
+
 @task
 def link(ctx):
     u"""install vim config"""
     vimrc_cmd = "ln -s {pwd}/vimrc ~/.vimrc".format(pwd=BASE_DIR)
-    run(vimrc_cmd)
+    run_cmd(vimrc_cmd)
 
     vim_cmd = "ln -s {pwd}/vim ~/.vim".format(pwd=BASE_DIR)
-    run(vim_cmd)
+    run_cmd(vim_cmd)
 
 
 @task
-def backup(ctx):
+def bak(ctx):
     u"""backup old config"""
-    if path.exists("~/.vimrc"):
+    if path_exists("~/.vimrc"):
         cmd = "mv ~/.vimrc ~/.vimrc.bak.{date}".format(date=now_str())
-        run(cmd)
+        run_cmd(cmd)
 
-    if path.exists("~/.vim"):
+    if path_exists("~/.vim"):
         cmd = "mv ~/.vim ~/.vim.bak.{date}".format(date=now_str())
-        run(cmd)
+        run_cmd(cmd)
 
 
 @task
 def rm(ctx):
     u"""remove old config"""
-    if path.exists("~/.vimrc"):
+    if path_exists("~/.vimrc"):
         cmd = "rm ~/.vimrc"
-        run(cmd)
+        run_cmd(cmd)
 
-    if path.exists("~/.vim"):
+    if path_exists("~/.vim"):
         cmd = "rm -rf ~/.vim"
-        run(cmd)
+        run_cmd(cmd)
 
 
 @task
 def plugin(ctx):
     u"""install plugin"""
-    run("git submodule update --init --recursive")
-    run("vim +PluginInstall +qall")
-    run("cd vim/bundle/YouCompleteMe && ./install.py")
+    run_cmd("git submodule update --init --recursive")
+    run_cmd("vim +PluginInstall +qall")
+    run_cmd("cd vim/bundle/YouCompleteMe && ./install.py")
 
 
 @task
 def install(ctx, backup=False):
     if backup:
-        backup(ctx)
+        bak(ctx)
     else:
         rm(ctx)
 
